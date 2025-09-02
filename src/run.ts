1a95c485b40fdf703ec285f9a4b5c5f5da9f9546
U-GAIA\silas <silas.orting@pm.me>
2025-09-02 11:14:36 +0200

    Fix PowerShell command quoting + cmd.exe env specification

--- src/run.ts
+++ src/run.ts
@@ -33,6 +33,33 @@ function escapeArg(arg: string): string {
   return escape(arg);
 }
 
+function escapeCommand(command: string): string {
+  const shellPath = vscode.env.shell;
+  const shell = shellPath.toLowerCase();
+  if (shell.includes('powershell')) {
+    // PS does not like a quoted path, instead we quote each part except the drive letter...
+    return command.split(/[/\\]/).map((value: string, index: number, array: string[]) => {
+      return index === 0 ? value : escapePowerShellArg(value);
+    }).join("/");
+  } else if (shell.includes('cmd.exe')) {
+    // cmd.exe is happy to run a quoted path
+    return escapeCmdArg(command);
+  }
+  // Bash is also happy with a quoted path
+  return escapeBashArg(command);
+}
+
+function getShellName(): string {
+  const shellPath = vscode.env.shell;
+  const shell = shellPath.toLowerCase();
+  if (shell.includes('powershell')) {
+    return 'powershell';
+  } else if (shell.includes('cmd.exe')) {
+    return 'cmd';
+  }
+  return 'bash';
+}
+
 /**
  * Builds a shell-specific command with environment variable
  */
@@ -41,19 +68,17 @@ function buildCommandWithEnv(
   params: string[],
   envVarName: string,
   envVarValue: string,
-  shellPath: string
+  shell: string
 ): string {
-  const shell = shellPath.toLowerCase();
-
   let escape: (arg: string) => string;
   let envPrefix: string;
 
-  if (shell.includes('powershell')) {
+  if (shell === 'powershell') {
     escape = escapePowerShellArg;
     envPrefix = `$env:${envVarName} = ${escape(envVarValue)}; `;
-  } else if (shell.includes('cmd.exe')) {
+  } else if (shell === 'cmd') {
     escape = escapeCmdArg;
-    envPrefix = `set ${envVarName}=${envVarValue} && `;
+    envPrefix = `set ${envVarName}=${envVarValue}&&`;
   } else {
     // Default to Bash
     escape = escapeBashArg;
@@ -61,7 +86,8 @@ function buildCommandWithEnv(
   }
 
   const escapedParams = params.map(escape).join(' ');
-  const escapedCommand = escape(command);
+  // We need to escape the command as well, but it is not the same way as for an argument
+  const escapedCommand = escapeCommand(command);
   return `${envPrefix}${escapedCommand} ${escapedParams}`;
 }
 
@@ -76,10 +102,11 @@ export function runCommandWithEnv(
   envVarValue: string,
   terminalName = 'Daisy'
 ) {
-  const shellPath = vscode.env.shell;
-  const fullCommand = buildCommandWithEnv(command, params, envVarName, envVarValue, shellPath);
+  const shell = getShellName();
 
-  const terminal = getOrCreateTerminal(terminalName);
+  const fullCommand = buildCommandWithEnv(command, params, envVarName, envVarValue, shell);
+
+  const terminal = getOrCreateTerminal(`${terminalName} ${shell}`);
   terminal.show();
   if (directoryExists(workingDirectory)) {
     terminal.sendText(`cd ${escapeArg(workingDirectory)}`); // Change directory if valid
